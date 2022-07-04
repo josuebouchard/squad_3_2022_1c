c9344745cb12f9b96151c5a586e775390d10e96b
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2022-07-03 23:06:12 -0300

    removed one to many fields from ticket update

--- psa_soporte/schemas.py
+++ psa_soporte/schemas.py
@@ -39,12 +39,10 @@ class TicketUpdate(BaseModel):
     title: str
     description: str
     clientId: int
-    tasks: List[int]
     priority: str  # PRIORIDAD_ALTA, PRIORIDAD_MEDIA, PRIORIDAD_BAJA
     severity: str  # S1, S2, S3, S4
     state: str  # ABIERTO, CERRADO, EN_PROGRESO
     deadline: datetime
-    employees: List[int]
 
     class Config:
         orm_mode = True
